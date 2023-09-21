2fa71d095dce8865c1bcf46d9348314d61834ebb
eshizhan <eshizhan@126.com>
2023-09-21 23:58:24 +0800

    support get javadoc description from getter method

--- springdoc-openapi-javadoc/src/main/java/org/springdoc/openapi/javadoc/JavadocPropertyCustomizer.java
+++ springdoc-openapi-javadoc/src/main/java/org/springdoc/openapi/javadoc/JavadocPropertyCustomizer.java
@@ -22,11 +22,11 @@
 
 package org.springdoc.openapi.javadoc;
 
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.databind.JavaType;
@@ -48,98 +48,113 @@ import org.springframework.util.CollectionUtils;
  */
 public class JavadocPropertyCustomizer implements ModelConverter {
 
-	/**
-	 * The Javadoc provider.
-	 */
-	private final JavadocProvider javadocProvider;
+    /**
+     * The Javadoc provider.
+     */
+    private final JavadocProvider javadocProvider;
 
-	/**
-	 * The Object mapper provider.
-	 */
-	private final ObjectMapperProvider objectMapperProvider;
+    /**
+     * The Object mapper provider.
+     */
+    private final ObjectMapperProvider objectMapperProvider;
 
-	/**
-	 * Instantiates a new Javadoc property customizer.
-	 *
-	 * @param javadocProvider the javadoc provider
-	 * @param objectMapperProvider the object mapper provider
-	 */
-	public JavadocPropertyCustomizer(JavadocProvider javadocProvider, ObjectMapperProvider objectMapperProvider) {
-		this.javadocProvider = javadocProvider;
-		this.objectMapperProvider = objectMapperProvider;
-	}
+    /**
+     * Instantiates a new Javadoc property customizer.
+     *
+     * @param javadocProvider the javadoc provider
+     * @param objectMapperProvider the object mapper provider
+     */
+    public JavadocPropertyCustomizer(JavadocProvider javadocProvider, ObjectMapperProvider objectMapperProvider) {
+        this.javadocProvider = javadocProvider;
+        this.objectMapperProvider = objectMapperProvider;
+    }
 
-	/**
-	 * Resolve schema.
-	 *
-	 * @param type the type
-	 * @param context the context
-	 * @param chain the chain
-	 * @return the schema
-	 */
-	@Override
-	public Schema resolve(AnnotatedType type, ModelConverterContext context, Iterator<ModelConverter> chain) {
-		if (chain.hasNext()) {
-			JavaType javaType = objectMapperProvider.jsonMapper().constructType(type.getType());
-			if (javaType != null) {
-				Class<?> cls = javaType.getRawClass();
-				Schema<?> resolvedSchema = chain.next().resolve(type, context, chain);
-				List<Field> fields = FieldUtils.getAllFieldsList(cls);
-				if (!CollectionUtils.isEmpty(fields)) {
-					if (!type.isSchemaProperty()) {
-						Schema existingSchema = context.resolve(type);
-						setJavadocDescription(cls, fields, existingSchema);
-					}
-					else if (resolvedSchema != null && resolvedSchema.get$ref() != null && resolvedSchema.get$ref().contains(AnnotationsUtils.COMPONENTS_REF)) {
-						String schemaName = resolvedSchema.get$ref().substring(21);
-						Schema existingSchema = context.getDefinedModels().get(schemaName);
-						setJavadocDescription(cls, fields, existingSchema);
-					}
-				}
-				return resolvedSchema;
-			}
-		}
-		return null;
-	}
+    /**
+     * Resolve schema.
+     *
+     * @param type the type
+     * @param context the context
+     * @param chain the chain
+     * @return the schema
+     */
+    @Override
+    public Schema resolve(AnnotatedType type, ModelConverterContext context, Iterator<ModelConverter> chain) {
+        if (chain.hasNext()) {
+            JavaType javaType = objectMapperProvider.jsonMapper().constructType(type.getType());
+            if (javaType != null) {
+                Class<?> cls = javaType.getRawClass();
+                Schema<?> resolvedSchema = chain.next().resolve(type, context, chain);
+                List<Field> fields = FieldUtils.getAllFieldsList(cls);
+                List<PropertyDescriptor> clsProperties = new ArrayList<>();
+                try {
+                    clsProperties = Arrays.asList(Introspector.getBeanInfo(cls).getPropertyDescriptors());
+                } catch (IntrospectionException ignored) {}
+                if (!CollectionUtils.isEmpty(fields) || !CollectionUtils.isEmpty(clsProperties)) {
+                    if (!type.isSchemaProperty()) {
+                        Schema existingSchema = context.resolve(type);
+                        setJavadocDescription(cls, fields, clsProperties, existingSchema);
+                    }
+                    else if (resolvedSchema != null && resolvedSchema.get$ref() != null && resolvedSchema.get$ref().contains(AnnotationsUtils.COMPONENTS_REF)) {
+                        String schemaName = resolvedSchema.get$ref().substring(21);
+                        Schema existingSchema = context.getDefinedModels().get(schemaName);
+                        setJavadocDescription(cls, fields, clsProperties, existingSchema);
+                    }
+                }
+                return resolvedSchema;
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * Sets javadoc description.
-	 *
-	 * @param cls the cls
-	 * @param fields the fields
-	 * @param existingSchema the existing schema
-	 */
-	void setJavadocDescription(Class<?> cls, List<Field> fields, Schema existingSchema) {
-		if (existingSchema != null) {
-			if (StringUtils.isBlank(existingSchema.getDescription())) {
-				existingSchema.setDescription(javadocProvider.getClassJavadoc(cls));
-			}
-			Map<String, Schema> properties = existingSchema.getProperties();
-			if (!CollectionUtils.isEmpty(properties)) {
-				if (cls.getSuperclass() != null && "java.lang.Record".equals(cls.getSuperclass().getName())) {
-					Map<String, String> recordParamMap = javadocProvider.getRecordClassParamJavadoc(cls);
-					properties.entrySet().stream()
-							.filter(stringSchemaEntry -> StringUtils.isBlank(stringSchemaEntry.getValue().getDescription()))
-							.forEach(stringSchemaEntry -> {
-								if (recordParamMap.containsKey(stringSchemaEntry.getKey()))
-									stringSchemaEntry.getValue().setDescription(recordParamMap.get(stringSchemaEntry.getKey()));
-							});
-				}
+    /**
+     * Sets javadoc description.
+     *
+     * @param cls the cls
+     * @param fields the fields
+     * @param clsProperties the bean properties of cls
+     * @param existingSchema the existing schema
+     */
+    void setJavadocDescription(Class<?> cls, List<Field> fields, List<PropertyDescriptor> clsProperties, Schema existingSchema) {
+        if (existingSchema != null) {
+            if (StringUtils.isBlank(existingSchema.getDescription())) {
+                existingSchema.setDescription(javadocProvider.getClassJavadoc(cls));
+            }
+            Map<String, Schema> properties = existingSchema.getProperties();
+            if (!CollectionUtils.isEmpty(properties)) {
+                if (cls.getSuperclass() != null && "java.lang.Record".equals(cls.getSuperclass().getName())) {
+                    Map<String, String> recordParamMap = javadocProvider.getRecordClassParamJavadoc(cls);
+                    properties.entrySet().stream()
+                            .filter(stringSchemaEntry -> StringUtils.isBlank(stringSchemaEntry.getValue().getDescription()))
+                            .forEach(stringSchemaEntry -> {
+                                if (recordParamMap.containsKey(stringSchemaEntry.getKey()))
+                                    stringSchemaEntry.getValue().setDescription(recordParamMap.get(stringSchemaEntry.getKey()));
+                            });
+                }
 
-				properties.entrySet().stream()
-						.filter(stringSchemaEntry -> StringUtils.isBlank(stringSchemaEntry.getValue().getDescription()))
-						.forEach(stringSchemaEntry -> {
-							Optional<Field> optionalField = fields.stream().filter(field1 -> field1.getName().equals(stringSchemaEntry.getKey())).findAny();
-							optionalField.ifPresent(field -> {
-								String fieldJavadoc = javadocProvider.getFieldJavadoc(field);
-								if (StringUtils.isNotBlank(fieldJavadoc))
-									stringSchemaEntry.getValue().setDescription(fieldJavadoc);
-							});
-						});
-			}
-			fields.stream().filter(f -> f.isAnnotationPresent(JsonUnwrapped.class))
-					.forEach(f -> setJavadocDescription(f.getType(), FieldUtils.getAllFieldsList(f.getType()), existingSchema));
+                properties.entrySet().stream()
+                        .filter(stringSchemaEntry -> StringUtils.isBlank(stringSchemaEntry.getValue().getDescription()))
+                        .forEach(stringSchemaEntry -> {
+                            fields.stream()
+                                    .filter(field1 -> field1.getName().equals(stringSchemaEntry.getKey()))
+                                    .findAny().ifPresent(field -> {
+                                        String fieldJavadoc = javadocProvider.getFieldJavadoc(field);
+                                        if (StringUtils.isNotBlank(fieldJavadoc))
+                                            stringSchemaEntry.getValue().setDescription(fieldJavadoc);
+                            });
+                            if (StringUtils.isBlank(stringSchemaEntry.getValue().getDescription())) {
+                                clsProperties.stream()
+                                        .filter(pd -> pd.getName().equals(stringSchemaEntry.getKey()))
+                                        .findAny().ifPresent(pd1 -> {
+                                            String fieldJavadoc = javadocProvider.getMethodJavadocDescription(pd1.getReadMethod());
+                                            if (StringUtils.isNotBlank(fieldJavadoc))
+                                                stringSchemaEntry.getValue().setDescription(fieldJavadoc);
+                                });
+                            }
+                        });
+            }
+            fields.stream().filter(f -> f.isAnnotationPresent(JsonUnwrapped.class))
+                    .forEach(f -> setJavadocDescription(f.getType(), FieldUtils.getAllFieldsList(f.getType()), clsProperties, existingSchema));
 
-		}
-	}
+        }
+    }
 }
